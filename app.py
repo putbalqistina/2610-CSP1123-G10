758db6d5dfd18099300cc3c5f1ef721f01671dc6
Aminah Mazlan <aminahmazlan@gmail.com>
2026-06-04 22:38:51 +0800

    edit

--- app.py
+++ app.py
@@ -476,6 +476,13 @@ def delete_file(filename):
 
     return redirect(request.referrer or url_for('dashboard'))
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(
+        app.config['UPLOAD_FOLDER'],
+        filename
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
