e259022fce28a455c46b2afed8756b88f946adb2
Puteri Balqis <putbalqistina@gmail.com>
2026-07-02 01:12:42 +0800

    fixing email

--- app.py
+++ app.py
@@ -19,10 +19,12 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
 app.config["MAIL_PORT"] = int(os.environ.get("MAIL_PORT"))
-app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS") == "True"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+app.config["MAIL_TIMEOUT"] = 20
 
 mail = Mail(app)
 # Ensure upload folder directory structure exists on startup
